d9e32b92a7ebf3144b35e71a5ca43c584ac89d86
nanher94 <hernan.albiol@gmail.com>
2022-07-29 19:20:20 +0000

    Generator Excuses

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.getElementById("excuse").innerHTML = generateExcuses();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuses = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
